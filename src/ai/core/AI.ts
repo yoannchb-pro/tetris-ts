09343876e11299ec7be74396db138f11fc0419c5
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2023-08-11 22:44:55 -0400

    AI setup

---
+++ src/ai/core/AI.ts
@@ -0,0 +1,26 @@
+import GameHandler from "../../game/core/GameHandler";
+
+class AI {
+  private fps = 60;
+  private clock: NodeJS.Timeout;
+
+  constructor(private gameHandler: GameHandler) {}
+
+  main() {}
+
+  start() {
+    if (!this.clock) {
+      this.clock = setTimeout(() => this.main(), 1000 / this.fps);
+    }
+  }
+
+  stop() {
+    if (this.clock) clearTimeout(this.clock);
+  }
+
+  setFps(fps: number) {
+    this.fps = fps;
+  }
+}
+
+export default AI;
--- src/game/index.ts
+++ src/game/index.ts
@@ -2,11 +2,13 @@ import GameHandler from "./core/GameHandler";
 
 const canvas = document.querySelector("canvas");
 const playBtn = document.querySelector("#play");
-const stopBtn = document.querySelector("#stop");
+const pauseBtn = document.querySelector("#pause");
 const resetBtn = document.querySelector("#reset");
 
 const game = new GameHandler(canvas);
 
 playBtn.addEventListener("click", () => game.start());
-stopBtn.addEventListener("click", () => game.stop());
+pauseBtn.addEventListener("click", () => game.stop());
 resetBtn.addEventListener("click", () => game.reset());
+
+export default game;
